96e30480648e4fc6ca06a1f5061a201201857f6e
Nurul Jaelani <nuruljaelani96@gmail.com>
2022-07-12 09:54:10 +0700

    Update page

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { StaticImage } from 'gatsby-plugin-image';
 import {
-  AiFillGithub, AiFillLinkedin, AiFillInstagram, AiFillDribbbleCircle,
+  AiFillGithub,
+  AiFillLinkedin,
+  AiFillInstagram,
+  AiFillDribbbleCircle,
 } from 'react-icons/ai';
 import { Link } from 'gatsby';
 
@@ -9,26 +12,71 @@ function Nav({
   home, portfolio, skill, about, options,
 }) {
   return (
-    <nav className="from-slate-900 to-stone-900 bg-gradient-to-r px-4 lg:px-2" ref={home}>
+    <nav
+      className="from-slate-900 to-stone-900 bg-gradient-to-r px-4 lg:px-2"
+      ref={home}
+    >
       <div className="flex mx-auto max-w-5xl space-x-0 md:space-x-14 lg:space-x-20">
         <div className="md:basis-1/2 basis-0 rounded-b-3xl hidden md:block">
-          <StaticImage src="../images/Nurul Jaelani.jpg" alt="" className="rounded-b-full drop-shadow-md" />
+          <StaticImage
+            src="../images/Nurul Jaelani.jpg"
+            alt=""
+            className="rounded-b-full drop-shadow-md"
+          />
         </div>
         <div className="flex flex-col md:basis-1/2 basis-full space-y-8">
           <div className="flex text-white justify-between py-2 md:py-4">
-            <button type="button" onClick={() => home.current.scrollIntoView(options)}>Home</button>
-            <button type="button" onClick={() => portfolio.current.scrollIntoView(options)}>Portfolio</button>
-            <button type="button" onClick={() => skill.current.scrollIntoView(options)}>Skills</button>
-            <button type="button" onClick={() => about.current.scrollIntoView(options)}>About</button>
-            <StaticImage src="../images/nj2.svg" alt="" className="w-14 h-auto" />
+            <button
+              type="button"
+              onClick={() => home.current.scrollIntoView(options)}
+            >
+              Home
+            </button>
+            <button
+              type="button"
+              onClick={() => portfolio.current.scrollIntoView(options)}
+            >
+              Portfolio
+            </button>
+            <button
+              type="button"
+              onClick={() => skill.current.scrollIntoView(options)}
+            >
+              Skills
+            </button>
+            <button
+              type="button"
+              onClick={() => about.current.scrollIntoView(options)}
+            >
+              About
+            </button>
+            <StaticImage
+              src="../images/nj2.svg"
+              alt=""
+              className="w-14 h-auto"
+            />
           </div>
           <div className="flex flex-col space-y-2 md:space-y-4 lg:space-y-6">
-            <p className="text-lg text-stone-500 md:text-xl lg:text-2xl font-extralight italic">Nurul Jaelani</p>
-            <p className="text-white font-semibold text-2xl md:text-4xl lg:text-5xl">Fullstack Web Developer</p>
-            <p className="text-white">Fullstack web developer with 1year experience.</p>
+            <p className="text-lg text-stone-500 md:text-xl lg:text-2xl font-extralight italic">
+              Nurul Jaelani
+            </p>
+            <p className="text-white font-semibold text-2xl md:text-4xl lg:text-5xl">
+              Fullstack Web Developer
+            </p>
+            <p className="text-white">
+              Fullstack web developer with 1year experience.
+            </p>
           </div>
-          <Link to="https://drive.google.com/file/d/1uZGLTNcNTo1f58ETRcXwPgEyTQzKxIZE/view?usp=sharing" target="_blank">
-            <button type="button" className="bg-stone-200 rounded-3xl text-stone-800 w-1/3 shadow-md drop-shadow-md font-semibold p-2">Download CV</button>
+          <Link
+            to="https://drive.google.com/file/d/1VbxVR1Swj1uEubJ1BWBviOMOEHvP3Vmd/view?usp=sharing"
+            target="_blank"
+          >
+            <button
+              type="button"
+              className="bg-stone-200 rounded-3xl text-stone-800 w-1/3 shadow-md drop-shadow-md font-semibold p-2"
+            >
+              Download CV
+            </button>
           </Link>
           <div className="space-x-2 md:space-x-3 flex">
             <Link to="https://github.com/nuruljaelani" target="_blank">
@@ -36,9 +84,6 @@ function Nav({
             </Link>
             <Link to="https://linkedin.com/in/nurul-jaelani" target="_blank">
               <AiFillLinkedin className="text-white text-3xl" />
-            </Link>
-            <Link to="https://instagram.com/aga_jelekk" target="_blank">
-              <AiFillInstagram className="text-white text-3xl" />
             </Link>
             <Link to="https://dribbble.com/nuruljaelani" target="_blank">
               <AiFillDribbbleCircle className="text-white text-3xl" />
--- src/components/Skill.js
+++ src/components/Skill.js
@@ -8,36 +8,36 @@ function Skill({ skill }) {
         <div className="flex flex-col px-4 md:px-6 py-4 md:py-10 space-y-4 sm:space-y-6 md:space-y-8 lg:space-y-14">
           <p className="text-white text-xl md:text-2xl lg:text-3xl font-semibold underline decoration-2 underline-offset-8 text-center">Skill</p>
           <div className="grid grid-cols-2 md:grid-cols-4 gap-2 md:gap-4">
-            <div className="flex bg-yellow-100 rounded-3xl py-2 px-4 items-center space-x-2">
-              <StaticImage src="../images/javascript.svg" alt="" className="w-6 md:w-10 h-auto" />
+            <div className="flex rounded-3xl py-2 px-4 items-center space-x-2">
+              <StaticImage src="../images/javascript.svg" alt="" className="w-6 md:w-10 lg:w-14 h-auto" />
               <p className="text-yellow-800 text-sm sm:text-base md:text-lg font-semibold">Javascript</p>
             </div>
-            <div className="flex bg-sky-100 rounded-3xl py-2 px-4 items-center space-x-2">
-              <StaticImage src="../images/php.svg" alt="" className="w-6 md:w-10 h-auto" />
+            <div className="flex rounded-3xl py-2 px-4 items-center space-x-2">
+              <StaticImage src="../images/php.svg" alt="" className="w-6 md:w-10 lg:w-14 h-auto" />
               <p className="text-sky-800 text-sm sm:text-base md:text-lg font-semibold">Php</p>
             </div>
-            <div className="flex bg-rose-100 rounded-3xl py-2 px-4 items-center space-x-2">
-              <StaticImage src="../images/laravel.svg" alt="" className="w-6 md:w-10 h-auto" />
+            <div className="flex rounded-3xl py-2 px-4 items-center space-x-2">
+              <StaticImage src="../images/laravel.svg" alt="" className="w-6 md:w-10 lg:w-14 h-auto" />
               <p className="text-rose-800 text-sm sm:text-base md:text-lg font-semibold">Laravel</p>
             </div>
-            <div className="flex bg-green-100 rounded-3xl py-2 px-4 items-center space-x-2">
-              <StaticImage src="../images/nodejs-icon.svg" alt="" className="w-6 md:w-10 h-auto" />
+            <div className="flex rounded-3xl py-2 px-4 items-center space-x-2">
+              <StaticImage src="../images/nodejs-icon.svg" alt="" className="w-6 md:w-10 lg:w-14 h-auto" />
               <p className="text-green-800 text-sm sm:text-base md:text-lg font-semibold">Node Js</p>
             </div>
-            <div className="flex bg-emerald-100 rounded-3xl py-2 px-4 items-center space-x-2">
-              <StaticImage src="../images/vuejs-seeklogo.com.svg" alt="" className="w-6 md:w-10 h-auto" />
+            <div className="flex rounded-3xl py-2 px-4 items-center space-x-2">
+              <StaticImage src="../images/vuejs-seeklogo.com.svg" alt="" className="w-6 md:w-10 lg:w-14 h-auto" />
               <p className="text-emerald-800 text-sm sm:text-base md:text-lg font-semibold">Vue Js</p>
             </div>
-            <div className="flex bg-cyan-100 rounded-3xl py-2 px-4 items-center space-x-2">
-              <StaticImage src="../images/react.svg" alt="" className="w-6 md:w-10 h-auto" />
+            <div className="flex rounded-3xl py-2 px-4 items-center space-x-2">
+              <StaticImage src="../images/react.svg" alt="" className="w-6 md:w-10 lg:w-14 h-auto" />
               <p className="text-cyan-800 text-sm sm:text-base md:text-lg font-semibold">React Js</p>
             </div>
-            <div className="flex bg-sky-100 rounded-3xl py-2 px-4 items-center space-x-2">
-              <StaticImage src="../images/Go-Logo_Blue.png" alt="" className="w-6 md:w-10 h-auto" />
+            <div className="flex rounded-3xl py-2 px-4 items-center space-x-2">
+              <StaticImage src="../images/Go-Logo_Blue.png" alt="" className="w-6 md:w-10 lg:w-14 h-auto" />
               <p className="text-sky-800 text-sm sm:text-base md:text-lg font-semibold">Golang</p>
             </div>
-            <div className="flex bg-red-100 rounded-3xl py-2 px-4 items-center space-x-2">
-              <StaticImage src="../images/git-seeklogo.com.svg" alt="" className="w-6 md:w-10 h-auto" />
+            <div className="flex rounded-3xl py-2 px-4 items-center space-x-2">
+              <StaticImage src="../images/git-seeklogo.com.svg" alt="" className="w-6 md:w-10 lg:w-14 h-auto" />
               <p className="text-red-800 text-sm sm:text-base md:text-lg font-semibold">Git</p>
             </div>
           </div>
