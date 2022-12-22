466be4ae1d438e545c918f1eb74a3a86c77a15d6
Nurul Jaelani <nuruljaelani96@gmail.com>
2022-12-22 09:51:24 +0700

    update link

--- src/components/Slide.js
+++ src/components/Slide.js
@@ -50,18 +50,12 @@ function Slide() {
         </SplideSlide>
         <SplideSlide>
           <div className="flex flex-col space-y-4">
-            <Link to="https://bukukita.vercel.app" target="_blank">
+            <Link to="https://buku-id.vercel.app" target="_blank">
               <StaticImage src="../images/bukuid.png" alt="" className="rounded-[1.5rem] md:rounded-2xl drop-shadow-lg shadow-current shadow-md h-40 lg:h-48" />
             </Link>
             <div className="flex flex-col space-y-2">
               <div className="flex space-x-2 items-center">
                 <div className="bg-sky-500 p-1 rounded-md text-white text-xs w-fit">Frontend</div>
-                <Link to="https://github.com/nuruljaelani/bookstore.fe" target="_blank">
-                  <div className="flex items-center space-x-1">
-                    <AiFillGithub className="text-white" />
-                    <p className="text-xs text-white">Repository</p>
-                  </div>
-                </Link>
               </div>
               <p className="text-white font-semibold text-base md:text-lg">Toko Buku Online</p>
             </div>
